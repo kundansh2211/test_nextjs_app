bef0cd9a1270a4d6488f04b0aff34d592b8e8382
Kundan <kundanims2020@gmail.com>
2025-04-22 10:04:22 +0530

    Update test.tsx

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -14,6 +14,8 @@ function RedundantStateComponent() {
       <p>Count: {count}</p>
       <p>Double Count: {doubleCount}</p>
       <button onClick={increment}>Increment</button>
+      <button onClick={increment}>Deccrement</button>
+      
     </div>
   );
 }
